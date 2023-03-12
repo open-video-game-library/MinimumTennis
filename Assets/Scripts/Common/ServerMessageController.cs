159be76c57fcea81b4d9b5542dd91ca6cae91630
KazuyaIida <2001.kazuya9@gmail.com>
2023-03-12 18:02:16 +0900

    Change tennis stadium model.

--- Assets/Scripts/Common/ServerMessageController.cs
+++ Assets/Scripts/Common/ServerMessageController.cs
@@ -9,6 +9,12 @@ public class ServerMessageController : MonoBehaviour
 
     private bool tossed;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
--- Assets/Scripts/Home/CompetitionStartButton.cs
+++ Assets/Scripts/Home/CompetitionStartButton.cs
@@ -6,5 +6,8 @@ public class CompetitionStartButton : MonoBehaviour
     public void OnClickCompetitionButton()
     {
         SceneManager.LoadScene("CompetitionScene");
+
+        PlayerColor.playerColor = new Color32(255, 255, 255, 255);
+        OpponentColor.opponentColor = new Color32(30, 30, 30, 255);
     }
 }
